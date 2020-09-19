8bb05294245712ebca879d1b8d085a7ea9fbbd42
Chad <chadracelis@yahoo.com>
2020-09-19 15:30:41 -0400

    #cjmux8 - Improve editchannel UI

--- src/app/services/AppService.ts
+++ src/app/services/AppService.ts
@@ -162,7 +162,7 @@ export class AppService {
       }
     
       this.carrierService.init();
-      // this.native.setRootRouter(['/editserverinfo']);
+      // this.native.setRootRouter(['/eidtchannel']);
       this.native.setRootRouter(['/tabs/home']);
       this.feedService.updateSignInDataExpTime(signInData);
     }
